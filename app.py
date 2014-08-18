ac798a2ad93ea2d9410738c69df3e52756f519e0
David <kido@seas.upenn.edu>
2014-08-17 17:18:27 -0700

    deleted the app.run part

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask
 
 # initialization
 app = Flask(__name__)
-app.config.update(
-	DEBUG = True,
-)
 
 # controllers
 @app.route("/")
 def hello():
 	return "Hello from Python!"
 
+'''
 # launch
 if __name == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
+
+'''
